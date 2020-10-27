d05401e957b37fa14948959356a387bcbfcdff86
superboy <8xsuperboyx8@gmail.com>
2020-10-27 08:59:36 +0545

    test2

--- sql/db.php
+++ sql/db.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "cryptog";
+$host = "us-cdbr-east-02.cleardb.com";
+$user = "b5a05a1316d338";
+$pass = "7668a5e6";
+$dbname = "heroku_eaa15dcb615fcf4";
 
 $conn = mysqli_connect($host, $user, $pass, $dbname);
 
